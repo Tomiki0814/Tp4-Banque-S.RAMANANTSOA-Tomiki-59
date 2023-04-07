8f60227d126917abad66ce5759a5724178c15b63
Tomiki0814 <tomikiramanantsoa@gmail.com>
2023-04-07 08:09:33 +0300

    creation table via entity

---
+++ src/main/java/mg/itu/tp4banque/ejb/GestionnaireCompte.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tp4banque.ejb;
+
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import mg.itu.tp4banque.entities.CompteBancaire;
+
+/**
+ *
+ * @author tomik
+ */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",
+    password="mysql008",
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true"
+    }
+)
+@Stateless
+public class GestionnaireCompte {
+    @PersistenceContext(unitName = "banquePU")
+private EntityManager em;
+
+public void persist(CompteBancaire compteBancaire) {
+  em.persist(compteBancaire);
+}
+}
